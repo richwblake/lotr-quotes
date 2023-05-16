2a26a808346313366c1e5876ef96ea7a760041b1
Wills Blake <richwblake@gmail.com>
2023-05-16 12:50:18 -0400

    initial :

---
+++ src/db.js
@@ -0,0 +1,94 @@
+const quotes = {
+  "characters": [
+    {
+      "name": "Frodo Baggins",
+      "race": "Hobbit",
+      "weapon": "Sting",
+      "status": "Alive",
+      "quotes": [
+        "I will take the Ring, though I do not know the way.",
+        "I wish the Ring had never come to me. I wish none of this had happened.",
+        "Even the smallest person can change the course of the future."
+      ]
+    },
+    {
+      "name": "Aragorn",
+      "race": "Human",
+      "weapon": "Andúril",
+      "status": "Alive",
+      "quotes": [
+        "A day may come when the courage of men fails, but it is not this day!",
+        "I do not fear death. I fear not doing enough before I die.",
+        "I am Aragorn, son of Arathorn. This is the hour of my birthright!"
+      ]
+    },
+    {
+      "name": "Gandalf",
+      "race": "Maia",
+      "weapon": "Glamdring",
+      "status": "Alive",
+      "quotes": [
+        "All we have to decide is what to do with the time that is given to us.",
+        "You shall not pass!",
+        "The greatest adventure is what lies ahead."
+      ]
+    },
+    {
+      "name": "Legolas",
+      "race": "Elf",
+      "weapon": "Bow and arrows",
+      "status": "Alive",
+      "quotes": [
+        "A red sun rises. Blood has been spilled this night.",
+        "I feel something. A slight tingle in my fingers. I think it's affecting me!",
+        "The way is shut. It was made by those who are dead, and the dead keep it."
+      ]
+    },
+    {
+      "name": "Gimli",
+      "race": "Dwarf",
+      "weapon": "Battle Axe",
+      "status": "Alive",
+      "quotes": [
+        "I will be dead before I see the Ring in the hands of an Elf!",
+        "Nobody tosses a dwarf!",
+        "Certainty of death, small chance of success... What are we waiting for?"
+      ]
+    },
+    {
+      "name": "Boromir",
+      "race": "Human",
+      "weapon": "Sword",
+      "status": "Deceased",
+      "quotes": [
+        "One does not simply walk into Mordor!",
+        "I would have followed you, my brother... my captain... my king.",
+        "I tried to take the Ring from Frodo. I am sorry. I have paid."
+      ]
+    },
+    {
+      "name": "Samwise Gamgee",
+      "race": "Hobbit",
+      "weapon": "Elven-made Blade",
+      "status": "Alive",
+      "quotes": [
+        "I can't carry it for you, but I can carry you!",
+        "There's some good in this world, Mr. Frodo, and it's worth fighting for.",
+        "It's like in the great stories, Mr. Frodo. The ones that really mattered."
+      ]
+    },
+    {
+      "name": "Gollum",
+      "race": "Hobbit",
+      "weapon": "None",
+      "status": "Alive",
+      "quotes": [
+        "My precious!",
+        "We wants it, we needs it. Must have the precious!",
+        "Leave now and never come back!"
+      ]
+    }
+  ]
+}
+
+export default quotes;
